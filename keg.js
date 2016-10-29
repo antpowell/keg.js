120952dd4963ce1268ab2c5612f10a74821736e8
Julian LaNeve <lanevejulian@gmail.com>
2016-10-29 14:32:00 -0500

    Add basic CRUD logging

--- keg.js
+++ keg.js
@@ -1,2 +1,22 @@
 // keg.js
-console.log('test');
+
+var keg;
+
+keg.create = function(object) {
+    console.log('create', object);
+}
+
+keg.read = function(selector) {
+    console.log('read', selector);
+}
+
+keg.update = function(object, newObject) {
+    console.log('update', object, newObject);
+}
+
+keg.delete = function(selector) {
+    console.log('delete', selector);
+}
+
+
+exports.keg = keg;
